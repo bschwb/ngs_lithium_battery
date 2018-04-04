0249d0b613a99e110e86ee6d5a596cc287e125a5
Bernd Schwarzenbacher <bernd.schwb@yahoo.com>
2018-04-05 00:06:40 +0200

    Add capped off variants of open-circuit potentials

--- battery.py
+++ battery.py
@@ -79,10 +79,23 @@ def open_circuit_manganese(concentration):
     concentr = concentration / normalization_concentration
     a = 4.19829
     b = 0.0565661 * tanh(-14.5546*concentr + 8.60942)
-    c = 0.0275479 * (1/Pow(0.998432-concentr, 0.492465) - 1.90111)
+    z = 0.99839
+    c = ngs.IfPos(z - concentr, 0.0275479 * (1/Pow(0.998432-concentr, 0.492465) - 1.90111),
+                  0.0275479 * (1/Pow(0.998432-z, 0.492465) - 1.90111))
     d = 0.157123 * exp(-0.04738 * concentr**8)
     e = 0.810239 * exp(-40*(concentr-0.133875))
-    return a + b - c - d + e
+
+    lower_cutoff = 0.05
+    e2 = 0.810239 * exp(-40*(lower_cutoff-0.133875))
+
+    f = 0.5-0.5*tanh(1000*(concentr - z))
+    g = 0.0275479 * (1/pow(0.998432-z, 0.492465) - 1.90111)
+
+    r = 0.5+0.5*tanh(1000*(concentr - lower_cutoff))
+
+    # return a + b - c * f - (1-f) * g - d + e
+    k = (a + b - c * f - (1-f) * g - d + e)
+    return r * k + (1-r) * (a+e2)
 
 
 def open_circuit_carbon(concentration):
@@ -93,7 +106,11 @@ def open_circuit_carbon(concentration):
     normalized concentration range: [0, 0.7]
     """
     concentr = concentration / normalization_concentration
-    return -0.16 + 1.32 * exp(-3 * concentr)
+    # return -0.16 + 1.32 * exp(-3 * concentr)
+    lower_capoff = -1
+    r = 0.5+0.5*tanh(100*(concentr - lower_capoff))
+
+    return (-0.16 + 1.32 * exp(-3 * concentr)) * r + (1-r) * (-0.16 + 1.32*exp(-3*lower_capoff))
 
 
 def charge_flux_prefactor_cathode(concentration):
